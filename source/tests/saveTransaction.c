f3b4905f625209b40dcf66e4b11a66385cf7521e
anthony.asadu <anthony.asadu@DEVAPP45>
2021-01-06 15:02:56 +0100

    Create DB and Table Test Suites

--- source/tests/saveTransaction.c
+++ source/tests/saveTransaction.c
@@ -13,7 +13,7 @@ int main(int argc, char * argv[])
     if(nRet == 0)
     {
         TransactionData transaction;
-        memset(&transaction, 0, sizeof(TransactionData));
+        memset(&transaction, '0', sizeof(TransactionData));
         transaction.szStatus = false;
         strcpy(transaction.szRefNumber, argv[2]);
         
